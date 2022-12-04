f1681e902f93bbc319f222c96a1cb3b18c745500
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2022-12-04 16:04:19 +0100

    [2022] - Day 3 - Part 2

--- advent-of-code-2022/src/main/kotlin/com/yonatankarp/adventofcode/year2022/Day03.kt
+++ advent-of-code-2022/src/main/kotlin/com/yonatankarp/adventofcode/year2022/Day03.kt
@@ -4,13 +4,21 @@ import com.yonatankarp.adventofcode.utils.readPuzzleInput
 
 fun main() {
     println("The sum of priorities of all 🎁 is ${calculatePriorities()}")
+    println("The sum of priorities of all 🎁 for 🧝🧝🧝 is ${calculateElvesPriorities()}")
 }
 
 fun calculatePriorities(input: List<String> = readPuzzleInput(3)): Int {
     val priorities = (('a'..'z').zip(1..26) + ('A'..'Z').zip(27..52)).toMap()
     return input
         .map { it.substring(0, it.length / 2) to it.substring(it.length / 2) }
-        .map { it.first.filter { c -> it.second.contains(c) }.toSet() }
-        .flatMap { it.map { c -> priorities[c]!! } }
-        .sumOf { it }
+        .flatMap { it.first.filter { c -> it.second.contains(c) }.toSet() }
+        .sumOf { priorities[it]!! }
+}
+
+fun calculateElvesPriorities(input: List<String> = readPuzzleInput(3)): Int {
+    val priorities = (('a'..'z').zip(1..26) + ('A'..'Z').zip(27..52)).toMap()
+    return input
+        .chunked(3)
+        .flatMap { it.first().filter { c -> c in it[1] && c in it[2] }.toSet() }
+        .sumOf { priorities[it]!! }
 }
