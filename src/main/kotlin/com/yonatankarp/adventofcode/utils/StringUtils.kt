1193e045cde545e2e4e4968b97e007c09a4df81d
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2022-12-08 07:43:29 +0100

    Add `padStartWith` and `padEndWith` to StringUtils
    This commit adds a new string functionality that allows to append X amount of characters at the beginning of a string.

--- src/main/kotlin/com/yonatankarp/adventofcode/utils/StringUtils.kt
+++ src/main/kotlin/com/yonatankarp/adventofcode/utils/StringUtils.kt
@@ -16,3 +16,21 @@ fun String.hasDuplicates(ignoreCases: Boolean = true): Boolean {
         }
         .let { return false }
 }
+
+/**
+ * Padding the start of this string and return the result.
+ *
+ * @param number the amount of padding required
+ * @param padChar the character to pad with.
+ */
+fun String.padStartWith(number: Int, padChar: Char = ' '): String =
+        (this as CharSequence).padStart(number + this.length, padChar).toString()
+
+/**
+ * Padding the end of this string and return the result.
+ *
+ * @param number the amount of padding required
+ * @param padChar the character to pad with.
+ */
+fun String.padEndWith(number: Int, padChar: Char = ' '): String =
+    (this as CharSequence).padEnd(number + this.length, padChar).toString()
--- src/test/kotlin/utils/StringUtilsTest.kt
+++ src/test/kotlin/utils/StringUtilsTest.kt
@@ -1,6 +1,9 @@
 package utils
 
 import com.yonatankarp.adventofcode.utils.hasDuplicates
+import com.yonatankarp.adventofcode.utils.padEndWith
+import com.yonatankarp.adventofcode.utils.padStartWith
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -19,5 +22,25 @@ class StringUtilsTest {
     @Test
     fun `should return false for string with no duplications`() {
         assertFalse("Corn".hasDuplicates(ignoreCases = false))
+    }
+
+    @Test
+    fun `should pad the beginning of a string`() {
+        assertEquals("---ABC", "ABC".padStartWith(3, '-'))
+    }
+
+    @Test
+    fun `should pad the beginning of a string with none visible char`() {
+        assertEquals("\tABC", "ABC".padStartWith(1, '\t'))
+    }
+
+    @Test
+    fun `should pad the end of a string`() {
+        assertEquals("ABC---", "ABC".padEndWith(3, '-'))
+    }
+
+    @Test
+    fun `should pad the end of a string with none visible char`() {
+        assertEquals("ABC\t", "ABC".padEndWith(1, '\t'))
     }
 }
